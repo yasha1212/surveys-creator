0e2b979a55f217efb5025793140c1bb8569a3298
yasha1212 <doenglish1@mail.ru>
2021-01-10 19:02:30 +0300

    Add application version output for debug builds only

---
+++ Utils/AppVersionUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace Utils
+{
+    public static class AppVersionUtil
+    {
+        public static string GetAppVersion()
+        {
+            return Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
+        }
+    }
+}

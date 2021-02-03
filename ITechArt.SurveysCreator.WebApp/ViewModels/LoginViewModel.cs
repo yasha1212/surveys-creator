be503953789a0dfb3803aa420957c3533ef5a8ea
yasha1212 <doenglish1@mail.ru>
2021-02-03 16:03:28 +0300

    Add email syntax validation

--- ITechArt.SurveysCreator.WebApp/ViewModels/LoginViewModel.cs
+++ ITechArt.SurveysCreator.WebApp/ViewModels/LoginViewModel.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ITechArt.SurveysCreator.WebApp.ViewModels
 {
     public class LoginViewModel
     {
         [Required]
+        [EmailAddress(ErrorMessage = "Current email is not valid!")]
         public string Email { get; set; }
 
         [Required]

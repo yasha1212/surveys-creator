a9bcddab0b01d91b3baa70639ad779b5a808c007
yasha1212 <doenglish1@mail.ru>
2021-02-13 13:59:32 +0300

    Fix database tables naming bug

--- ITechArt.SurveysCreator.DAL/SurveysCreatorDbContext.cs
+++ ITechArt.SurveysCreator.DAL/SurveysCreatorDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ITechArt.SurveysCreator.DAL.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace ITechArt.SurveysCreator.DAL
@@ -9,5 +10,18 @@ namespace ITechArt.SurveysCreator.DAL
         public SurveysCreatorDbContext(DbContextOptions<SurveysCreatorDbContext> options)
             : base(options)
         { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>().ToTable("Users");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+        }
     }
 }
